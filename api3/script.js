b88e802d3e8baa0beb2c43ec02f22ccaaeb512e4
Juan Jeldres <jjeldresbiz@gmail.com>
2026-05-30 20:07:53 -0400

    boton funcionalidad aleatoria falta linkear con ids

--- api3/script.js
+++ api3/script.js
@@ -10,7 +10,10 @@ function mostrarDatos (quote) {
 fetch('https://dummyjson.com/quotes/random')
 .then(Response => Response.json())
 .then(datos => {
-    
-        mostrarDatos(datos.quote)
-        
+        mostrarDatos(datos.quote)       
     }).catch (error => console.log)
+const boton = document.getElementById('btn-frase')
+boton.addEventListener("click", () => {
+    const idAleatorio = Math.floor(Math.random() * 1430) + 1
+    console.log(idAleatorio) //el boton tira numero aleatorio hasta el 1430 pero no conecta aun con el id de la frase
+})
