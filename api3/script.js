9a6be5c1dfdffa5b2268d173a514f0abdd0e3845
Juan Jeldres <jjeldresbiz@gmail.com>
2026-05-31 15:59:17 -0400

    API 3 DONE hell yeah

--- api3/script.js
+++ api3/script.js
@@ -12,8 +12,21 @@ fetch('https://dummyjson.com/quotes/random')
 .then(datos => {
         mostrarDatos(datos.quote)       
     }).catch (error => console.log)
+
 const boton = document.getElementById('btn-frase')
 boton.addEventListener("click", () => {
-    const idAleatorio = Math.floor(Math.random() * 1430) + 1
+    const idAleatorio = Math.floor(Math.random() * 1454) + 1
     console.log(idAleatorio) //el boton tira numero aleatorio hasta el 1430 pero no conecta aun con el id de la frase
+    fetch(`https://dummyjson.com/quotes/${idAleatorio}`)
+    .then(Response => Response.json())
+    .then(datos => {
+        console.log(datos);
+        mostrarDatos(datos.quote)
+    })
 })
+
+/* fetch('https://dummyjson.com/quotes')
+    .then(response => response.json())
+    .then(datos => {
+        console.log(datos.total);
+    }); */
